3a3a773e6b0f234f85697f6455c0b440ad8b5b40
“mkosecki1 <mkosecki@op.pl>
2018-06-13 21:02:08 +0200

    update

---
+++ src/main/java/com/codewars/kyu8/HowDoICompareNumbers/HowDoICompare.java
@@ -0,0 +1,17 @@
+package com.codewars.kyu8.HowDoICompareNumbers;
+
+public class HowDoICompare {
+    public static String whatIs(Integer x) {
+        for (Object[] p : specialNumbers) {
+            if (p[0].equals(x))
+                return (String)p[1];
+        }
+        return "nothing";
+    }
+
+    static final Object[][] specialNumbers = {
+            {42, "everything"},
+            {42 * 42, "everything squared"},
+    };
+
+}
--- src/main/java/com/codewars/kyu8/TripleTrouble/TripleTroubleKata.java
+++ src/main/java/com/codewars/kyu8/TripleTrouble/TripleTroubleKata.java
@@ -2,12 +2,10 @@ package com.codewars.kyu8.TripleTrouble;
 
 public class TripleTroubleKata {
     public static String tripleTrouble(String one, String two, String three) {
-        String sum = one.substring(0,1) + two.substring(0,1) + three.substring(0,1) +
-                one.substring(1,2) + two.substring(1,2) + three.substring(1,2) +
-                one.substring(2,3) + two.substring(2,3) + three.substring(2,3)
-                + one.substring(3) + two.substring(3) + three.substring(3);
-        System.out.println("--> "+ sum);
-        return sum;
-
+        StringBuilder stringBuilder = new StringBuilder(one.length()*3);
+        for (int n=0; n<one.length();n++){
+            stringBuilder.append(one.charAt(n)).append(two.charAt(n)).append(three.charAt(n));
+        }
+        return stringBuilder.toString();
     }
 }
